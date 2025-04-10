71ca4a47e4263eb09be9ba2ac032a5da17f724d0
Jose Mogrovejo <j.mogrovejotorres@gmail.com>
2025-04-09 22:47:16 -0400

    fix: puerto y host para render

--- app.py
+++ app.py
@@ -43,6 +43,10 @@ def estado():
                            tiempo_retiro=tiempo_retiro)
 
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Render usa una variable PORT
+    app.run(host='0.0.0.0', port=port)
+
 
